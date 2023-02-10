49957c6d3c3d49ffe9356d467b1f1b037d7b5b84
Juan Opazo <dopazo178@gmail.com>
2023-02-10 10:57:25 -0500

    add dashboard template

---
+++ client/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+import Dashboard from "@/components/dashboard/Dashboard";
+
+const About = () => {
+    return (
+        <div>
+            <Dashboard/>
+        </div>
+    )
+}
+
+export default About;
